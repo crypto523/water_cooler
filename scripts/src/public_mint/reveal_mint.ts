ccf840530503bd5fd06a640efae40e38e5a0447f
crypto523 <jacksoncurry46@gmail.com>
2024-07-03 16:20:41 +0300

    make reveal_mint ptb

---
+++ scripts/src/public_mint/reveal_mint.ts
@@ -0,0 +1,57 @@
+import { Transaction } from '@mysten/sui/transactions';
+import { client, user1_keypair } from '../helpers.js';
+import data from '../../deployed_objects.json';
+import user_data from '../user_objects.json';
+
+const keypair = user1_keypair();
+
+const packageId = data.packageId;
+const mint_cap = user_data.user_objects.mint_cap;
+const mint = user_data.user_objects.mint;
+const attributes = user_data.user_objects.attribute;
+const image = user_data.user_objects.image;
+
+function stringToUint8Array(str: string): Uint8Array {
+    return new TextEncoder().encode(str);
+}
+
+(async () => {
+    const txb = new Transaction();
+
+    let key1 = "asd";
+    const keys = txb.makeMoveVec({
+        type: `0x1::string::String`,
+        elements: [txb.pure.string(key1)]
+    });
+    
+    let value1 = "asd";
+    const values = txb.makeMoveVec({
+        type: `0x1::string::String`,
+        elements: [txb.pure.string(value1)]
+    });
+
+    console.log("User1 execute reveal_mint");
+
+    txb.moveCall({
+        target: `${packageId}::mint::reveal_mint`,
+        arguments: [
+            txb.object(mint_cap),
+            txb.object(mint),
+            txb.object(attributes),
+            txb.object(image),
+            txb.pure.string(key1),
+        ],
+    });
+
+    const { objectChanges } = await client.signAndExecuteTransaction({
+        signer: keypair,
+        transaction: txb,
+        options: { showObjectChanges: true }
+    });
+
+    if (!objectChanges) {
+        console.log("Error: objectChanges is null or undefined");
+        process.exit(1);
+    }
+    console.log(objectChanges);
+})();
